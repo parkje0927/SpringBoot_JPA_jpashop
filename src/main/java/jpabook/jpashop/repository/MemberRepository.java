267062735922949f1721caccdd85769b49e620c6
jh <parkje09270@gmail.com>
2021-06-28 20:38:36 +0900

    16. 회원 서비스 개발

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,27 +1,29 @@
 package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
     // entityManager 를 만들어서 주입시켜준다.
-    @PersistenceContext
-    private EntityManager em;
+//    @PersistenceContext -> 이거 대신 Autowired 을 적어도 가능한 이유는 Spring JPA 가 이를 지원하기 때문이다.
+    private final EntityManager em;
 
     public void save(Member member) {
         em.persist(member);
         // 저장하고 나면 command, query 를 분리하라
         // Id 정도만 조회
+        //영속성 context 에 pk 값이 들어간다.
 //        return member.getId();
     }
 
-    public Member find(Long id) {
+    public Member findOne(Long id) {
         return em.find(Member.class, id);
     }
 
---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,78 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+// '읽기' 에서는 readonly=true 를 설정해주면 성능이 향상된다.
+// 따라서 '쓰기' 의 경우에만 @Transactional 을 걸어준다.
+@Transactional(readOnly = true)
+//생성자를 만들어준다.
+//@AllArgsConstructor
+
+//final 이 적혀있는 것만 생성자를 만들어줘서 AllArgsConstructor 이거보다 더 낫다.
+@RequiredArgsConstructor
+
+public class MemberService {
+
+    /**
+     * 1) field injection
+     * 변경이 불가능하다.
+     */
+//    @Autowired
+//    private MemberRepository memberRepository;
+
+    /**
+     * 2) setter injection
+     * 동작을 잘 하고 있는데 변경할 일이 없으므로 setter injection 은 좋지 않다.
+     */
+//    private MemberRepository memberRepository;
+//    @Autowired
+//    public void setMemberRepository(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+
+    /**
+     * 3) constructor injection
+     * set 으로 변경이 되지 않고, test case 작성 시 유용
+     * final 을 넣음으로써 문법 check 가능
+     * RequiredArgsConstructor 를 활용해서 생성자 코드 안 써도 된다.
+     */
+    private final MemberRepository memberRepository;
+
+//    public MemberService(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+
+    /**
+     * 회원가입
+     */
+    @Transactional
+    public Long join(Member member) {
+        //중복 검사
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalMonitorStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    //회원 전체 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
