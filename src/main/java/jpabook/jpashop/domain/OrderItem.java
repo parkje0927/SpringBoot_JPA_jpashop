a3d10118dceed5cde89cb67b6034d748a5c14ecb
jh <parkje09270@gmail.com>
2021-06-29 12:06:33 +0900

    21. 주문, 주문상품 엔티티 개발

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -12,7 +12,8 @@ import static javax.persistence.FetchType.LAZY;
 @Getter @Setter
 public class OrderItem {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     @Column(name = "order_item_id")
     private Long id;
 
@@ -31,5 +32,28 @@ public class OrderItem {
     private int orderPrice; //주문 당시의 가격
     private int count; //주문 당시의 수량
 
+    //==생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
 
+        // 재고 변경
+        item.removeStock(count);
+
+        return orderItem;
+    }
+
+    //==비즈니스 로직==//
+    public void cancel() {
+        // 재고를 가져와서 수량을 원복해준다.
+        getItem().addStock(count);
+    }
+
+    //==조회 로직==//
+    /** 주문상품 전체 가격 조회 */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
