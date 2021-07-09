6ec00d5a61a8063f3ee1a0047e2406ba49f65db2
jh <parkje09270@gmail.com>
2021-07-09 11:20:36 +0900

    29. 상품 등록

---
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -0,0 +1,38 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/items/new")
+    public String createForm(Model model) {
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping("/items/new")
+    public String create(BookForm form) {
+        Book book = new Book();
+
+        //setter 를 제거하고 static 생성자 메서드를 만들어서 사용하는 것이 더 좋다.
+        //실무에서는 setter 를 사용하지 않으려고 한다.
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/"; //저장된 책 목록으로 이동한다.
+    }
+}
