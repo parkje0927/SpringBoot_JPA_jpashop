fb601624ea314633137de4e4da9d45a05970ba60
jh <parkje09270@gmail.com>
2021-07-16 10:18:52 +0900

    회원 수정 API

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -75,4 +75,15 @@ public class MemberService {
     public Member findOne(Long memberId) {
         return memberRepository.findOne(memberId);
     }
+
+    @Transactional
+    public void update(Long id, String name) {
+        Member member = memberRepository.findOne(id);
+        member.setName(name); //변경 감지
+
+        /*
+         * Member 를 반환하도록 코드를 만들 경우, command 와 query 가 같이 있는 꼴이 된다.
+         * id 정도만 반환하는 건 괜찮다.
+         */
+    }
 }
