647cb78dd8345ec73bff72fadddddc598478a303
jh <parkje09270@gmail.com>
2021-07-31 21:50:44 +0900

    간단한 주문 조회 V2: 엔티티를 DTO로 변환(1)

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,13 +1,18 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * XToOne(ManyToOne, OneToOne)
@@ -58,5 +63,50 @@ public class OrderSimpleApiController {
             order.getDelivery().getAddress(); //getAddress 까지 하면 LAZY 강제 초기화해서 Delivery db 에 접근함
         }
         return all;
+    }
+
+    /**
+     * V2. 엔티티를 조회해서 DTO 로 변환 (fetch join 사용 x)
+     * - 단점 : 지연로딩으로 쿼리 N 번 호출
+     *
+     * 엔티티를 DTO 로 변환하는 일반적인 방법이다.
+     * 쿼리가 총 1 + N + N 번 실행된다. (v1 과 쿼리수 결과는 같다.)
+     *   - order 조회 1번(order 조회 결과 수가 N 이 된다.)
+     *   - order -> member 지연 로딩 조회 N 번
+     *   - order -> delivery 지연 로딩 조회 N 번
+     *   예시) order 의 결과가 4개면 최악의 경우 1 + 4 + 4 번 실행된다. (최악의 경우)
+     *   지연 로딩은 영속성 컨텍스트에서 조회하므로, 이미 조회된 경우 쿼리를 생략한다.
+     *
+     * 처음에 order 조회하면 -> sql 1번 실행해서 -> 결과 주문 수가 2개가 나온다.
+     * 그러면 loop 도는 곳에서 2번 돈다.
+     * 그러므로 총 5번 돈다. 
+     *
+     */
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        //아래에서 loop 돌릴 때, LAZY 초기화
+        List<SimpleOrderDto> result = orders.stream()
+                .map(SimpleOrderDto::new)
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName(); //LAZY 초기화
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress(); //LAZY 초기화
+        }
     }
 }
